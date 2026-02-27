f5ac53c892c815b404d914e23da3739340961848
BrunoL12 <brunolinares@gmail.com>
2026-02-27 11:04:16 -0500

    Se agrega el Proyecto

---
+++ src/config/clients.ts
@@ -0,0 +1,8 @@
+import { env } from "./env";
+import { HttpClient } from "../core/http/httpClient";
+
+export const clients = {
+  health: new HttpClient(env.baseUrlHealth),
+  playground: new HttpClient(env.baseUrlPlayground),
+  login: new HttpClient(env.baseUrlLogin),
+};
